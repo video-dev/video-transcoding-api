0aed609483d5c3bba0368c5f682c2b506339c474
Francisco Souza <francisco.souza@nytimes.com>
2016-01-19 16:05:40 -0500

    main: fix import path for the service package

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"github.com/NYTimes/gizmo/examples/servers/json/service"
-
 	"github.com/NYTimes/gizmo/config"
 	"github.com/NYTimes/gizmo/server"
+	"github.com/nytm/video-transcoding-api/service"
 )
 
 func main() {
-	// showing 1 way of managing gizmo/config: importing from a local file
 	var cfg *service.Config
 	config.LoadJSONFile("./config.json", &cfg)
 	config.LoadEnvConfig(&cfg)
 
 	server.Init("video-transcoding-api", cfg.Server)
-
 	err := server.Register(service.NewJSONService(cfg))
 	if err != nil {
 		server.Log.Fatal("unable to register service: ", err)
