c884c03f6a9b2174e6a72313f6d1819a20d9c656
Francisco Souza <francisco.souza@nytimes.com>
2016-02-02 15:43:17 -0500

    service: split getTranscodeJob and newTranscodeJob tests
    I know that these tests are _very_ similar, but I wanted to split them
    anyway :D

--- service/transcode_test.go
+++ service/transcode_test.go
@@ -70,8 +70,6 @@ func (d *fakeDB) GetJob(id string) (*db.Job, error) {
 func TestTranscode(t *testing.T) {
 	tests := []struct {
 		givenTestCase       string
-		givenURI            string
-		givenHTTPMethod     string
 		givenRequestBody    string
 		givenTriggerDBError bool
 
@@ -80,8 +78,6 @@ func TestTranscode(t *testing.T) {
 	}{
 		{
 			"New job with profile based encoding",
-			"/jobs",
-			"POST",
 			fmt.Sprintf(`{
   "source": "http://another.non.existent/video.mp4",
   "destination": "s3://some.bucket.s3.amazonaws.com/some_path",
@@ -97,8 +93,6 @@ func TestTranscode(t *testing.T) {
 		},
 		{
 			"New job with preset based encoding",
-			"/jobs",
-			"POST",
 			`{
   "source": "http://another.non.existent/video.mp4",
   "destination": "s3://some.bucket.s3.amazonaws.com/some_path",
@@ -114,8 +108,6 @@ func TestTranscode(t *testing.T) {
 		},
 		{
 			"New job with database error",
-			"/jobs",
-			"POST",
 			fmt.Sprintf(`{
   "source": "http://another.non.existent/video.mp4",
   "destination": "s3://some.bucket.s3.amazonaws.com/some_path",
@@ -131,8 +123,6 @@ func TestTranscode(t *testing.T) {
 		},
 		{
 			"New job with invalid provider",
-			"/jobs",
-			"POST",
 			fmt.Sprintf(`{
   "source": "http://another.non.existent/video.mp4",
   "destination": "s3://some.bucket.s3.amazonaws.com/some_path",
@@ -148,8 +138,6 @@ func TestTranscode(t *testing.T) {
 		},
 		{
 			"New job missing profiles and presets",
-			"/jobs",
-			"POST",
 			`{
   "source": "http://another.non.existent/video.mp4",
   "destination": "s3://some.bucket.s3.amazonaws.com/some_path",
@@ -164,8 +152,6 @@ func TestTranscode(t *testing.T) {
 		},
 		{
 			"New job with both profiles and presets",
-			"/jobs",
-			"POST",
 			fmt.Sprintf(`{
   "source": "http://another.non.existent/video.mp4",
   "destination": "s3://some.bucket.s3.amazonaws.com/some_path",
@@ -182,8 +168,6 @@ func TestTranscode(t *testing.T) {
 		},
 		{
 			"New job with unsupported profile-based",
-			"/jobs",
-			"POST",
 			fmt.Sprintf(`{
   "source": "http://another.non.existent/video.mp4",
   "destination": "s3://some.bucket.s3.amazonaws.com/some_path",
@@ -199,8 +183,6 @@ func TestTranscode(t *testing.T) {
 		},
 		{
 			"New job with unsupported preset-based",
-			"/jobs",
-			"POST",
 			`{
   "source": "http://another.non.existent/video.mp4",
   "destination": "s3://some.bucket.s3.amazonaws.com/some_path",
@@ -212,38 +194,6 @@ func TestTranscode(t *testing.T) {
 			http.StatusBadRequest,
 			map[string]interface{}{
 				"error": `Provider "profile-fake" does not support preset-based encoding`,
-			},
-		},
-
-		{
-			"Get job",
-			"/jobs/12345",
-			"GET",
-			"",
-			false,
-
-			http.StatusOK,
-			map[string]interface{}{
-				"providerJobId": "provider-job-123",
-				"status":        "finished",
-				"providerName":  "fake",
-				"statusMessage": "The job is finished",
-				"providerStatus": map[string]interface{}{
-					"progress":   100.0,
-					"sourcefile": "http://some.source.file",
-				},
-			},
-		},
-		{
-			"Get job with inexistent job id",
-			"/jobs/non_existent_job",
-			"GET",
-			"",
-			false,
-
-			http.StatusNotFound,
-			map[string]interface{}{
-				"error": "Error retrieving job with id 'non_existent_job': job not found",
 			},
 		},
 	}
@@ -262,11 +212,75 @@ func TestTranscode(t *testing.T) {
 				"preset-fake":  presetFakeProviderFactory,
 			},
 		})
-		r, _ := http.NewRequest(
-			test.givenHTTPMethod,
-			test.givenURI,
-			strings.NewReader(test.givenRequestBody),
-		)
+		r, _ := http.NewRequest("POST", "/jobs", strings.NewReader(test.givenRequestBody))
+		r.Header.Set("Content-Type", "application/json")
+		w := httptest.NewRecorder()
+		srvr.ServeHTTP(w, r)
+		if w.Code != test.wantCode {
+			t.Errorf("%s: expected response code of %d; got %d", test.givenTestCase, test.wantCode, w.Code)
+		}
+		var got interface{}
+		err := json.NewDecoder(w.Body).Decode(&got)
+		if err != nil {
+			t.Errorf("%s: unable to JSON decode response body: %s", test.givenTestCase, err)
+		}
+		if !reflect.DeepEqual(got, test.wantBody) {
+			t.Errorf("%s: expected response body of\n%#v;\ngot\n%#v", test.givenTestCase, test.wantBody, got)
+		}
+		// ** THIS IS REQUIRED in order to run the test multiple times.
+		metrics.DefaultRegistry.UnregisterAll()
+	}
+}
+
+func TestGetTranscodeJob(t *testing.T) {
+	tests := []struct {
+		givenTestCase       string
+		givenURI            string
+		givenTriggerDBError bool
+
+		wantCode int
+		wantBody interface{}
+	}{
+		{
+			"Get job",
+			"/jobs/12345",
+			false,
+
+			http.StatusOK,
+			map[string]interface{}{
+				"providerJobId": "provider-job-123",
+				"status":        "finished",
+				"providerName":  "fake",
+				"statusMessage": "The job is finished",
+				"providerStatus": map[string]interface{}{
+					"progress":   100.0,
+					"sourcefile": "http://some.source.file",
+				},
+			},
+		},
+		{
+			"Get job with inexistent job id",
+			"/jobs/non_existent_job",
+			false,
+
+			http.StatusNotFound,
+			map[string]interface{}{
+				"error": "Error retrieving job with id 'non_existent_job': job not found",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		srvr := server.NewSimpleServer(nil)
+		fakeDBObj := db.JobRepository(&fakeDB{
+			TriggerDBError: test.givenTriggerDBError,
+		})
+		srvr.Register(&TranscodingService{
+			config:    &config.Config{},
+			db:        fakeDBObj,
+			providers: map[string]provider.Factory{"fake": fakeProviderFactory},
+		})
+		r, _ := http.NewRequest("GET", test.givenURI, nil)
 		r.Header.Set("Content-Type", "application/json")
 		w := httptest.NewRecorder()
 		srvr.ServeHTTP(w, r)
