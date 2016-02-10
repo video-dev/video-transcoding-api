8a8f75bf045483fb5e1fb754cbd6238c4b987922
Francisco Souza <francisco.souza@nytimes.com>
2016-02-10 19:31:06 -0200

    db/types: add json tags to struct fields

--- db/types.go
+++ db/types.go
@@ -3,14 +3,17 @@ package db
 // Job represents the job that is persisted in the repository of the Transcoding
 // API.
 type Job struct {
-	ID            string `redis-hash:"-"`
-	ProviderName  string `redis-hash:"providerName"`
-	ProviderJobID string `redis-hash:"providerJobID"`
+	ID            string `redis-hash:"-" json:"jobId"`
+	ProviderName  string `redis-hash:"providerName" json:"providerName"`
+	ProviderJobID string `redis-hash:"providerJobID" json:"providerJobId"`
 }
 
 // Preset represents the preset that is persisted in the repository of the
 // Transcoding API.
+//
+// Each preset is just an aggregator of provider presets, where each preset in
+// the API maps to a preset on each provider.
 type Preset struct {
-	ID              string            `redis-hash:"-"`
-	ProviderMapping map[string]string `redis-hash:",expand"`
+	ID              string            `redis-hash:"-" json:"presetId"`
+	ProviderMapping map[string]string `redis-hash:",expand" json:"providerMapping"`
 }
