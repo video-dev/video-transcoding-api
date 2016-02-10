71f67004249039cdfa9c681b423a1bd3316a5b98
Francisco Souza <francisco.souza@nytimes.com>
2016-02-10 15:49:58 -0200

    swagger: add first version of the manifest
    Just the basic structure, let's explore it! :-)

---
+++ doc.go
@@ -0,0 +1,24 @@
+// video-transcoding-api
+//
+// HTTP API for transcoding media files into different formats using pluggable providers.
+//
+// Currently supported providers
+//
+// Amazon Elastic Transcoder: https://aws.amazon.com/elastictranscoder/
+// Elemental Conductor: https://www.elementaltechnologies.com/products/elemental-conductor
+// Encoding.com: http://api.encoding.com
+//
+// Schemes: http
+// BasePath: /
+// Version: 1.0.0
+// License: Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0.html
+//
+// Consumes:
+// - application/json
+//
+//
+// Produces:
+// - application/json
+//
+// swagger:meta
+package main
