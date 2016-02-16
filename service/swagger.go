161da6562222c6f30a69635e66a75568c786851a
Francisco Souza <francisco.souza@nytimes.com>
2016-02-16 16:18:24 -0200

    service: serve the swagger.json file
    We want to serve it with the proper CORS headers, so we support
    Swagger-UI and other pure-JavaScript clients.

---
+++ service/swagger.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func (s *TranscodingService) swaggerManifest(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile(s.config.SwaggerManifest)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// In order to support pure-JavaScript clients (like Swagger-UI), the
+	// server must set CORS headers.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	w.Write(data)
+}
