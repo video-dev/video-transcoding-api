4a62d00eff4c0a938ec1f67bf1dcaaf32891783b
Francisco Souza <francisco.souza@nytimes.com>
2016-03-03 14:42:50 -0300

    doc: minor improvement in markdown syntax

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ edit it.
 HTTP API for transcoding media files into different formats using pluggable
 providers.
 
-##Currently supported providers
+## Currently supported providers
 
 + [Amazon Elastic Transcoder](https://aws.amazon.com/elastictranscoder/)
 + [Elemental Conductor](https://www.elementaltechnologies.com/products/elemental-conductor)
--- doc.go
+++ doc.go
@@ -3,7 +3,7 @@
 // HTTP API for transcoding media files into different formats using pluggable
 // providers.
 //
-// ##Currently supported providers
+// ## Currently supported providers
 //
 // + [Amazon Elastic Transcoder](https://aws.amazon.com/elastictranscoder/)
 // + [Elemental Conductor](https://www.elementaltechnologies.com/products/elemental-conductor)
