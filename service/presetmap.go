924253eee523819493c8c5d80afcce1d67496152
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2016-06-13 12:55:46 -0400

    service: split preset/presetmaps in different files (closes #66)

---
+++ service/presetmap.go
@@ -0,0 +1,126 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/NYTimes/gizmo/web"
+	"github.com/nytm/video-transcoding-api/db"
+	"github.com/nytm/video-transcoding-api/swagger"
+)
+
+// swagger:route POST /presetmaps presets newPreset
+//
+// Creates a new preset in the API.
+//
+//     Responses:
+//       200: preset
+//       400: invalidPreset
+//       409: presetAlreadyExists
+//       500: genericError
+func (s *TranscodingService) newPresetMap(r *http.Request) swagger.GizmoJSONResponse {
+	var input newPresetMapInput
+	defer r.Body.Close()
+	preset, err := input.PresetMap(r.Body)
+	if err != nil {
+		return newInvalidPresetMapResponse(err)
+	}
+	err = s.db.CreatePresetMap(&preset)
+	switch err {
+	case nil:
+		return newPresetMapResponse(&preset)
+	case db.ErrPresetMapAlreadyExists:
+		return newPresetMapAlreadyExistsResponse(err)
+	default:
+		return swagger.NewErrorResponse(err)
+	}
+}
+
+// swagger:route GET /presetmaps/{name} presets getPreset
+//
+// Finds a preset using its name.
+//
+//     Responses:
+//       200: preset
+//       404: presetNotFound
+//       500: genericError
+func (s *TranscodingService) getPresetMap(r *http.Request) swagger.GizmoJSONResponse {
+	var params getPresetMapInput
+	params.loadParams(web.Vars(r))
+	preset, err := s.db.GetPresetMap(params.Name)
+
+	switch err {
+	case nil:
+		return newPresetMapResponse(preset)
+	case db.ErrPresetMapNotFound:
+		return newPresetMapNotFoundResponse(err)
+	default:
+		return swagger.NewErrorResponse(err)
+	}
+}
+
+// swagger:route PUT /presetmaps/{name} presets updatePreset
+//
+// Updates a presetmap using its name.
+//
+//     Responses:
+//       200: preset
+//       400: invalidPreset
+//       404: presetNotFound
+//       500: genericError
+func (s *TranscodingService) updatePresetMap(r *http.Request) swagger.GizmoJSONResponse {
+	defer r.Body.Close()
+	var input updatePresetMapInput
+	presetMap, err := input.PresetMap(web.Vars(r), r.Body)
+	if err != nil {
+		return newInvalidPresetMapResponse(err)
+	}
+	err = s.db.UpdatePresetMap(&presetMap)
+
+	switch err {
+	case nil:
+		updatedPresetMap, _ := s.db.GetPresetMap(presetMap.Name)
+		return newPresetMapResponse(updatedPresetMap)
+	case db.ErrPresetMapNotFound:
+		return newPresetMapNotFoundResponse(err)
+	default:
+		return swagger.NewErrorResponse(err)
+	}
+}
+
+// swagger:route DELETE /presetmaps/{name} presets deletePresetMap
+//
+// Deletes a presetmap by name.
+//
+//     Responses:
+//       200: emptyResponse
+//       404: presetNotFound
+//       500: genericError
+func (s *TranscodingService) deletePresetMap(r *http.Request) swagger.GizmoJSONResponse {
+	var params getPresetMapInput
+	params.loadParams(web.Vars(r))
+	err := s.db.DeletePresetMap(&db.PresetMap{Name: params.Name})
+
+	switch err {
+	case nil:
+		return emptyResponse(http.StatusOK)
+	case db.ErrPresetMapNotFound:
+		return newPresetMapNotFoundResponse(err)
+	default:
+		return swagger.NewErrorResponse(err)
+	}
+}
+
+// swagger:route GET /presetmaps presets listPresets
+//
+// List available presets on the API.
+//
+//     Responses:
+//       200: listPresetMaps
+//       500: genericError
+func (s *TranscodingService) listPresetMaps(r *http.Request) swagger.GizmoJSONResponse {
+	presetsMap, err := s.db.ListPresetMaps()
+	if err != nil {
+		return swagger.NewErrorResponse(err)
+	}
+	return newListPresetMapsResponse(presetsMap)
+}
---
+++ service/presetmap_params.go
@@ -0,0 +1,101 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/nytm/video-transcoding-api/db"
+	"github.com/nytm/video-transcoding-api/swagger"
+)
+
+// JSON-encoded preset returned on the newPreset and getPreset operations.
+//
+// swagger:response preset
+type presetMapResponse struct {
+	// in: body
+	Payload *db.PresetMap
+
+	baseResponse
+}
+
+func newPresetMapResponse(preset *db.PresetMap) *presetMapResponse {
+	return &presetMapResponse{
+		baseResponse: baseResponse{
+			payload: preset,
+			status:  http.StatusOK,
+		},
+	}
+}
+
+// error returned when the given preset name is not found on the API (either on
+// getPreset or deletePreset operations).
+//
+// swagger:response presetNotFound
+type presetMapNotFoundResponse struct {
+	// in: body
+	Error *swagger.ErrorResponse
+}
+
+func newPresetMapNotFoundResponse(err error) *presetMapNotFoundResponse {
+	return &presetMapNotFoundResponse{Error: swagger.NewErrorResponse(err).WithStatus(http.StatusNotFound)}
+}
+
+func (r *presetMapNotFoundResponse) Result() (int, interface{}, error) {
+	return r.Error.Result()
+}
+
+// error returned when the given preset data is not valid.
+//
+// swagger:response invalidPreset
+type invalidPresetMapResponse struct {
+	// in: body
+	Error *swagger.ErrorResponse
+}
+
+func newInvalidPresetMapResponse(err error) *invalidPresetMapResponse {
+	return &invalidPresetMapResponse{Error: swagger.NewErrorResponse(err).WithStatus(http.StatusBadRequest)}
+}
+
+func (r *invalidPresetMapResponse) Result() (int, interface{}, error) {
+	return r.Error.Result()
+}
+
+// error returned when trying to create a new preset using a name that is
+// already in-use.
+//
+// swagger:response presetAlreadyExists
+type presetMapAlreadyExistsResponse struct {
+	// in: body
+	Error *swagger.ErrorResponse
+}
+
+func newPresetMapAlreadyExistsResponse(err error) *presetMapAlreadyExistsResponse {
+	return &presetMapAlreadyExistsResponse{Error: swagger.NewErrorResponse(err).WithStatus(http.StatusConflict)}
+}
+
+func (r *presetMapAlreadyExistsResponse) Result() (int, interface{}, error) {
+	return r.Error.Result()
+}
+
+// response for the listPresetMaps operation. It's actually a JSON-encoded object
+// instead of an array, in the format `presetName: presetObject`
+//
+// swagger:response listPresetMaps
+type listPresetMapsResponse struct {
+	// in: body
+	PresetMaps map[string]db.PresetMap
+
+	baseResponse
+}
+
+func newListPresetMapsResponse(presetsMap []db.PresetMap) *listPresetMapsResponse {
+	Map := make(map[string]db.PresetMap, len(presetsMap))
+	for _, presetMap := range presetsMap {
+		Map[presetMap.Name] = presetMap
+	}
+	return &listPresetMapsResponse{
+		baseResponse: baseResponse{
+			status:  http.StatusOK,
+			payload: Map,
+		},
+	}
+}
