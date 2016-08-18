9532290ee8eadc1f5abf424db51f299072e9e847
Francisco Souza <francisco.souza@nytimes.com>
2016-08-18 17:39:30 -0400

    db: add a method for validating OutputOptions

---
+++ db/types_test.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestOutputOptionsValidation(t *testing.T) {
+	var tests = []struct {
+		testCase string
+		opts     OutputOptions
+		errMsg   string
+	}{
+		{
+			"valid options",
+			OutputOptions{
+				Label:     "mp4_720p",
+				Extension: "mp4",
+			},
+			"",
+		},
+		{
+			"missing label",
+			OutputOptions{
+				Label:     "",
+				Extension: "mp4",
+			},
+			"preset label is required",
+		},
+		{
+			"missing extension",
+			OutputOptions{
+				Label:     "mp4_720p",
+				Extension: "",
+			},
+			"extension is required",
+		},
+	}
+	for _, test := range tests {
+		err := test.opts.Validate()
+		if err == nil {
+			err = errors.New("")
+		}
+		if err.Error() != test.errMsg {
+			t.Errorf("wrong error message\nWant %q\nGot  %q", test.errMsg, err.Error())
+		}
+	}
+}
