145bf165771e2135c8f2b3bd1549a473b5e9814e
James Yeh <james.yeh@bitmovin.com>
2017-04-04 17:01:53 -0700

    adding debugging code will remove later

--- swagger/handler.go
+++ swagger/handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/NYTimes/gizmo/server"
+	"github.com/davecgh/go-spew/spew"
 )
 
 // GizmoJSONResponse represents a response type that can be converted to
@@ -20,6 +21,8 @@ type Handler func(*http.Request) GizmoJSONResponse
 // HandlerToJSONEndpoint converts a handler to a proper Gizmo JSONEndpoint.
 func HandlerToJSONEndpoint(h Handler) server.JSONEndpoint {
 	return func(r *http.Request) (int, interface{}, error) {
-		return h(r).Result()
+		a, b, c := h(r).Result()
+		spew.Dump(b)
+		return a, b, c
 	}
 }
