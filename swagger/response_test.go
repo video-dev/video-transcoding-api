7e47e625a1ce5c095a695a91024b89b42538e362
Francisco Souza <francisco.souza@nytimes.com>
2018-02-22 12:14:53 -0500

    swagger: make WithStatus ignore 0s

--- swagger/response_test.go
+++ swagger/response_test.go
@@ -37,6 +37,21 @@ func TestErrorResponseCustomStatus(t *testing.T) {
 	}
 }
 
+func TestErrorResponseZeroStatus(t *testing.T) {
+	internalError := errors.New("something went wrong")
+	errResp := NewErrorResponse(internalError).WithStatus(0)
+	code, data, err := errResp.Result()
+	if code != http.StatusInternalServerError {
+		t.Errorf("Wrong error code. Want %d. Got %d", http.StatusInternalServerError, code)
+	}
+	if data != nil {
+		t.Errorf("Unexpected non-nil data: %#v", data)
+	}
+	if err != errResp {
+		t.Errorf("Wrong error returned. Want %#v. Got %#v", errResp, err)
+	}
+}
+
 func TestErrorResponsErrorInterface(t *testing.T) {
 	var err error
 	msg := "something went wrong"
