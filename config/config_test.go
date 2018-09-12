d22ad46609a63c93ef1769ef99e3355c5490f61f
Francisco Souza <francisco.souza@nytimes.com>
2018-09-11 22:00:02 -0400

    config: workaround for gofmt issue
    Go 1.11 and 1.10 don't agree on this one.

--- config/config_test.go
+++ config/config_test.go
@@ -107,8 +107,9 @@ func TestLoadConfigFromEnv(t *testing.T) {
 			HTTPAccessLog: &accessLog,
 		},
 		Log: &logging.Config{
-			Level:                   "debug",
 			StackDriverErrorLogName: "error_log",
+
+			Level: "debug",
 		},
 	}
 	diff := cmp.Diff(*cfg, expectedCfg, cmpopts.IgnoreUnexported(server.Config{}))
@@ -206,7 +207,8 @@ func TestLoadConfigFromEnvWithDefaults(t *testing.T) {
 			HTTPAccessLog: &accessLog,
 		},
 		Log: &logging.Config{
-			Level:                   "info",
+			Level: "info",
+
 			StackDriverErrorLogName: "error_log",
 		},
 	}
