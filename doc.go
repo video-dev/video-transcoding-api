6436075866678bc42edc22581d9fc3b4710cebe9
Francisco Souza <fsouza@users.noreply.github.com>
2019-08-02 11:46:28 -0400

    doc: cleanup some old swagger stuff

--- doc.go
+++ doc.go
@@ -2,25 +2,4 @@
 //
 // HTTP API for transcoding media files into different formats using pluggable
 // providers.
-//
-// ## Currently supported providers
-//
-// + [Amazon Elastic Transcoder](https://aws.amazon.com/elastictranscoder/)
-// + [Elemental Conductor](https://www.elementaltechnologies.com/products/elemental-conductor)
-// + [Encoding.com](http://api.encoding.com)
-// + [Zencoder](http://zencoder.com)
-//
-// Schemes: http
-// BasePath: /
-// Version: 1.0.0
-// License: Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0.html
-//
-// Consumes:
-// - application/json
-//
-//
-// Produces:
-// - application/json
-//
-// swagger:meta
 package main
