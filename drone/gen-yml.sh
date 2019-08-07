3a0a2e012449d8d06d814c5ccc9c69e52842997d
Francisco Souza <fsouza@users.noreply.github.com>
2019-08-07 15:28:27 -0400

    drone: fix generation

--- drone/gen-yml.sh
+++ drone/gen-yml.sh
@@ -5,9 +5,10 @@ DIR=$(cd `dirname $0` && pwd -P)
 out_file=$(mktemp)
 
 (
+	echo "---"
 	echo "# DO NOT MODIFY - AUTO GENERATED"
 	echo
-	drone jsonnet --stream --stdout --source ${DIR}/ci.jsonnet
+	drone jsonnet --stream --stdout --source ${DIR}/ci.jsonnet | tail -n +2
 ) >$out_file
 
 mv $out_file ${DIR}/ci.yml
