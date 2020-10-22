4da72b8f80bf2df527c5afc3c9e185c1f9fb5d21
Evan Sonderegger <evan@rpy.xyz>
2020-10-22 14:21:39 -0400

    Set MinGOP in bitmovin preset when GopMode is fixed (#331)
    * Set MinGOP in bitmovin preset when GopMode is fixed
    
    * disable scenecut when in fixed GOP mode
    
    Co-authored-by: Evan Sonderegger <evan.sonderegger@nytimes.comm>
    Co-authored-by: Adam Sax <adam.sax@nytimes.com>

--- internal/provider/bitmovin/bitmovin.go
+++ internal/provider/bitmovin/bitmovin.go
@@ -227,6 +227,10 @@ func (p *bitmovinProvider) createH264VideoPreset(preset db.Preset, customData ma
 			return nil, err
 		}
 		h264.MaxGOP = intToPtr(int64(gopSize))
+		if preset.Video.GopMode == "fixed" {
+			h264.MinGOP = intToPtr(int64(gopSize))
+			h264.SceneCutThreshold = intToPtr(int64(0))
+		}
 	}
 	if preset.Video.BFrames != "" {
 		bFrames, err := strconv.Atoi(preset.Video.BFrames)
