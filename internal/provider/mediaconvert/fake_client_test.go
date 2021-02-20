da4d73532cac6fa52aa12d504b3509a8e4b004d6
francisco souza <fsouza@users.noreply.github.com>
2021-02-20 10:33:44 -0500

    mediaconvert: fix compatibility with latest aws-sdk-go-v2

--- internal/provider/mediaconvert/fake_client_test.go
+++ internal/provider/mediaconvert/fake_client_test.go
@@ -1,14 +1,14 @@
 package mediaconvert
 
 import (
-	"net/http"
+	"context"
 	"sync/atomic"
 	"testing"
 	"unsafe"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/retry"
 	"github.com/aws/aws-sdk-go-v2/service/mediaconvert"
+	"github.com/aws/aws-sdk-go-v2/service/mediaconvert/types"
 )
 
 // testMediaConvertClient is an implementation of the mediaconvertClient interface
@@ -23,98 +23,68 @@ type testMediaConvertClient struct {
 	cancelJobCalledWith    string
 	listJobsCalled         bool
 
-	jobReturnedByGetJob      mediaconvert.Job
+	jobReturnedByGetJob      types.Job
 	jobIDReturnedByCreateJob string
-	getPresetContainerType   mediaconvert.ContainerType
+	getPresetContainerType   types.ContainerType
 }
 
-func (c *testMediaConvertClient) CreatePresetRequest(input *mediaconvert.CreatePresetInput) mediaconvert.CreatePresetRequest {
+func (c *testMediaConvertClient) CreatePreset(_ context.Context, input *mediaconvert.CreatePresetInput, _ ...func(*mediaconvert.Options)) (*mediaconvert.CreatePresetOutput, error) {
 	c.createPresetCalledWith = input
-	return mediaconvert.CreatePresetRequest{
-		Request: &aws.Request{
-			Retryer:     retry.NewStandard(),
-			HTTPRequest: &http.Request{}, Data: &mediaconvert.CreatePresetOutput{
-				Preset: &mediaconvert.Preset{
-					Name: input.Name,
-					Settings: &mediaconvert.PresetSettings{
-						ContainerSettings: &mediaconvert.ContainerSettings{
-							Container: input.Settings.ContainerSettings.Container,
-						},
-					},
+	return &mediaconvert.CreatePresetOutput{
+		Preset: &types.Preset{
+			Name: input.Name,
+			Settings: &types.PresetSettings{
+				ContainerSettings: &types.ContainerSettings{
+					Container: input.Settings.ContainerSettings.Container,
 				},
 			},
 		},
-	}
+	}, nil
 }
 
-func (c *testMediaConvertClient) GetJobRequest(*mediaconvert.GetJobInput) mediaconvert.GetJobRequest {
-	return mediaconvert.GetJobRequest{Request: &aws.Request{
-		Retryer:     retry.NewStandard(),
-		HTTPRequest: &http.Request{},
-		Data: &mediaconvert.GetJobOutput{
-			Job: &c.jobReturnedByGetJob,
-		},
-	}}
+func (c *testMediaConvertClient) GetJob(context.Context, *mediaconvert.GetJobInput, ...func(*mediaconvert.Options)) (*mediaconvert.GetJobOutput, error) {
+	return &mediaconvert.GetJobOutput{
+		Job: &c.jobReturnedByGetJob,
+	}, nil
 }
 
-func (c *testMediaConvertClient) ListJobsRequest(*mediaconvert.ListJobsInput) mediaconvert.ListJobsRequest {
+func (c *testMediaConvertClient) ListJobs(context.Context, *mediaconvert.ListJobsInput, ...func(*mediaconvert.Options)) (*mediaconvert.ListJobsOutput, error) {
 	c.listJobsCalled = true
-	return mediaconvert.ListJobsRequest{Request: &aws.Request{
-		Retryer:     retry.NewStandard(),
-		HTTPRequest: &http.Request{},
-		Data:        &mediaconvert.ListJobsOutput{},
-	}}
+	return &mediaconvert.ListJobsOutput{}, nil
 }
 
-func (c *testMediaConvertClient) CreateJobRequest(input *mediaconvert.CreateJobInput) mediaconvert.CreateJobRequest {
+func (c *testMediaConvertClient) CreateJob(_ context.Context, input *mediaconvert.CreateJobInput, _ ...func(*mediaconvert.Options)) (*mediaconvert.CreateJobOutput, error) {
 	c.createJobCalledWith = *input
-	return mediaconvert.CreateJobRequest{
-		Request: &aws.Request{
-			Retryer:     retry.NewStandard(),
-			HTTPRequest: &http.Request{}, Data: &mediaconvert.CreateJobOutput{
-				Job: &mediaconvert.Job{
-					Id: aws.String(c.jobIDReturnedByCreateJob),
-				},
-			}},
-	}
+	return &mediaconvert.CreateJobOutput{
+		Job: &types.Job{
+			Id: aws.String(c.jobIDReturnedByCreateJob),
+		},
+	}, nil
 }
 
-func (c *testMediaConvertClient) CancelJobRequest(input *mediaconvert.CancelJobInput) mediaconvert.CancelJobRequest {
+func (c *testMediaConvertClient) CancelJob(_ context.Context, input *mediaconvert.CancelJobInput, _ ...func(*mediaconvert.Options)) (*mediaconvert.CancelJobOutput, error) {
 	c.cancelJobCalledWith = *input.Id
-	return mediaconvert.CancelJobRequest{Request: &aws.Request{
-		Retryer:     retry.NewStandard(),
-		HTTPRequest: &http.Request{},
-		Data:        &mediaconvert.CancelJobOutput{},
-	}}
+	return &mediaconvert.CancelJobOutput{}, nil
 }
 
-func (c *testMediaConvertClient) GetPresetRequest(input *mediaconvert.GetPresetInput) mediaconvert.GetPresetRequest {
+func (c *testMediaConvertClient) GetPreset(_ context.Context, input *mediaconvert.GetPresetInput, _ ...func(*mediaconvert.Options)) (*mediaconvert.GetPresetOutput, error) {
 	// atomically set the value of getPresetCalledWith to avoid data races,
 	// should probably take a different approach?
 	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&c.getPresetCalledWith)), unsafe.Pointer(input.Name))
 
-	return mediaconvert.GetPresetRequest{
-		Request: &aws.Request{
-			Retryer:     retry.NewStandard(),
-			HTTPRequest: &http.Request{}, Data: &mediaconvert.GetPresetOutput{
-				Preset: &mediaconvert.Preset{
-					Name: input.Name,
-					Settings: &mediaconvert.PresetSettings{
-						ContainerSettings: &mediaconvert.ContainerSettings{
-							Container: c.getPresetContainerType,
-						},
-					},
+	return &mediaconvert.GetPresetOutput{
+		Preset: &types.Preset{
+			Name: input.Name,
+			Settings: &types.PresetSettings{
+				ContainerSettings: &types.ContainerSettings{
+					Container: c.getPresetContainerType,
 				},
 			},
 		},
-	}
+	}, nil
 }
 
-func (c *testMediaConvertClient) DeletePresetRequest(input *mediaconvert.DeletePresetInput) mediaconvert.DeletePresetRequest {
+func (c *testMediaConvertClient) DeletePreset(_ context.Context, input *mediaconvert.DeletePresetInput, _ ...func(*mediaconvert.Options)) (*mediaconvert.DeletePresetOutput, error) {
 	c.deletePresetCalledWith = *input.Name
-	return mediaconvert.DeletePresetRequest{Request: &aws.Request{
-		Retryer:     retry.NewStandard(),
-		HTTPRequest: &http.Request{},
-		Data:        &mediaconvert.DeletePresetOutput{},
-	}}
+	return &mediaconvert.DeletePresetOutput{}, nil
 }
